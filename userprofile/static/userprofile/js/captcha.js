f64ed699ad36c1a6a23818fe9dd7dd543c0dc502
rYauheni <ra.yauheni@gmail.com>
2023-11-21 17:55:56 +0300

    added stylesheet to register.html template

--- userprofile/static/userprofile/js/captcha.js
+++ userprofile/static/userprofile/js/captcha.js
@@ -1,27 +1,3 @@
-//<script type="text/javascript">
-//
-//$('.js-captcha-refresh').click(function(){
-//    $form = $(this).parents('form');
-//
-//    $.getJSON($(this).data('url'), {}, function(json) {
-//        // This should update your captcha image src and captcha hidden input
-//    });
-//
-//    return false;
-//});
-//
-//$('.captcha').click(function () {
-//    $.getJSON("/captcha/refresh/", function (result) {
-//        $('.captcha').attr('src', result['image_url']);
-//        $('#id_captcha_0').val(result['key'])
-//    });
-//});
-//
-//</script>
-
-/*
-* Credits go to https://stackoverflow.com/a/20371801/8504344, https://stackoverflow.com/users/179024/mw
-*/
 $(document).ready(function () {
     // Add refresh button after field (this can be done in the template as well)
     $('img.captcha').after(
